ea049213b25c1e9c98e6406e8bca6462da631ecf
kelevv <127852616+kelevv@users.noreply.github.com>
2026-09-16 06:13:58 +0300

    Add schema and tables for restaurant management
    Created a schema and multiple tables for managing locations, staff, inventory, menu items, orders, reservations, and customer feedback, along with necessary foreign key constraints.

--- topic-04-ddl/ddl.sql
+++ topic-04-ddl/ddl.sql
@@ -28,3 +28,167 @@
 -- Add your DDL below this line
 
 
+CREATE schema ua_4778_manual;
+
+CREATE TABLE ua_4778_manual.locations
+(
+    "UniqueID" bigserial PRIMARY KEY,
+    "address"  VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE ua_4778_manual.staff
+(
+    "UniqueID"    bigserial PRIMARY KEY,
+    "role"        varchar(20) NOT NULL CHECK (role IN ('kitchen_staff', 'server', 'manager')),
+    "location_id" bigint      NOT NULL
+);
+
+CREATE TABLE ua_4778_manual.basic_inventory
+(
+    "UniqueID"    bigserial PRIMARY KEY,
+    "count"       bigint, --TBD: clarify the purpose of the field
+    "location_id" bigint NOT NULL
+
+);
+
+CREATE TABLE ua_4778_manual.menu_items
+(
+    "UniqueID"                 bigserial PRIMARY KEY,
+    "name"                     VARCHAR(100) NOT NULL,
+    "category"                 VARCHAR(20)  NOT NULL CHECK (category IN ('appetizer', 'main_course', 'dessert')),
+    "price_usd"                DECIMAL(10, 2) DEFAULT 0 CHECK (price_usd >= 0),
+    "preparation_time_minutes" INT            DEFAULT 0 CHECK ("preparation_time_minutes" >= 0),
+    "location_id"              BIGINT       NOT NULL
+
+);
+
+CREATE TABLE ua_4778_manual.orders
+(
+    "UniqueID"    bigserial PRIMARY KEY,
+    "type"        VARCHAR(15) NOT NULL CHECK ( type IN ('dine-in', 'takeaway', 'delivery')),
+    "status"      VARCHAR(20) NOT NULL CHECK ("status" IN
+                                              ('pending', 'confirmed', 'in_progress', 'completed', 'cancelled')),
+    "location_id" BIGINT      NOT NULL
+);
+
+CREATE TABLE ua_4778_manual.reservations
+(
+    "UniqueID"     bigserial PRIMARY KEY,
+    "date_time"    timestamp NOT NULL,
+    "table_number" INT, --TBD: control max size of the location
+    "location_id"  BIGINT    NOT NULL
+);
+
+CREATE TABLE ua_4778_manual.menu_items_ingredients
+(
+    "menu_item_id"  bigint,
+    "ingredient_id" bigint,
+    PRIMARY KEY ("menu_item_id", "ingredient_id")
+);
+
+CREATE TABLE ua_4778_manual.staff_orders
+(
+    "staff_id" bigint,
+    "order_id" bigint,
+    PRIMARY KEY ("staff_id", "order_id")
+);
+
+CREATE TABLE ua_4778_manual.menu_items_orders
+(
+    "menu_item_id" bigint,
+    "order_id"     bigint,
+    PRIMARY KEY ("menu_item_id", "order_id")
+);
+
+CREATE TABLE ua_4778_manual.shift_schedules
+(
+    "UniqueID"      bigserial PRIMARY KEY,
+    "shift_details" VARCHAR(300) NOT NULL,
+    "staff_id"      BIGINT       NOT NULL
+);
+
+CREATE TABLE ua_4778_manual.ingredients
+(
+    "UniqueID"           bigserial PRIMARY KEY,
+    "name"               VARCHAR(50) NOT NULL,
+    "quantity"           BIGINT DEFAULT 0,
+    "basic_inventory_id" BIGINT      NOT NULL
+);
+
+CREATE TABLE ua_4778_manual.ingredients_suppliers
+(
+    "ingredient_id" BIGINT,
+    "supplier_id"   BIGINT,
+    PRIMARY KEY ("ingredient_id", "supplier_id")
+);
+
+CREATE TABLE ua_4778_manual.suppliers
+(
+    "UniqueID" bigserial PRIMARY KEY,
+    "details"  VARCHAR(200)
+);
+
+CREATE TABLE ua_4778_manual.customer_feedback
+(
+    "UniqueID"       bigserial PRIMARY KEY,
+    "rating"         INT NOT NULL CHECK ("rating" IN (1, 2, 3, 4, 5)),
+    "comment"        VARCHAR(200),
+    "customer_info"  VARCHAR(200),
+    "order_id"       BIGINT,
+    "reservation_id" BIGINT
+        CHECK (
+            ("order_id" IS NOT NULL AND "reservation_id" IS NULL)
+                OR
+            ("order_id" IS NULL AND "reservation_id" IS NOT NULL)
+            )
+);
+
+ALTER TABLE ua_4778_manual.staff
+    ADD FOREIGN KEY ("location_id") REFERENCES ua_4778_manual.locations ("UniqueID");
+
+ALTER TABLE ua_4778_manual.basic_inventory
+    ADD FOREIGN KEY ("location_id") REFERENCES ua_4778_manual.locations ("UniqueID");
+
+ALTER TABLE ua_4778_manual.menu_items
+    ADD FOREIGN KEY ("location_id") REFERENCES ua_4778_manual.locations ("UniqueID");
+
+ALTER TABLE ua_4778_manual.orders
+    ADD FOREIGN KEY ("location_id") REFERENCES ua_4778_manual.locations ("UniqueID");
+
+ALTER TABLE ua_4778_manual.reservations
+    ADD FOREIGN KEY ("location_id") REFERENCES ua_4778_manual.locations ("UniqueID");
+
+ALTER TABLE ua_4778_manual.shift_schedules
+    ADD CONSTRAINT shift_schedules_staff_unique UNIQUE ("staff_id"),
+    ADD CONSTRAINT shift_schedules_staff_fk FOREIGN KEY ("staff_id") REFERENCES ua_4778_manual.staff ("UniqueID");
+
+ALTER TABLE ua_4778_manual.ingredients
+    ADD FOREIGN KEY ("basic_inventory_id") REFERENCES ua_4778_manual.basic_inventory ("UniqueID");
+
+ALTER TABLE ua_4778_manual.staff_orders
+    ADD FOREIGN KEY ("staff_id") REFERENCES ua_4778_manual.staff ("UniqueID"),
+    ADD FOREIGN KEY ("order_id") REFERENCES ua_4778_manual.orders ("UniqueID");
+
+ALTER TABLE ua_4778_manual.menu_items_ingredients
+    ADD FOREIGN KEY ("menu_item_id") REFERENCES ua_4778_manual.menu_items ("UniqueID"),
+    ADD FOREIGN KEY ("ingredient_id") REFERENCES ua_4778_manual.ingredients ("UniqueID");
+
+ALTER TABLE ua_4778_manual.menu_items_orders
+    ADD FOREIGN KEY ("menu_item_id") REFERENCES ua_4778_manual.menu_items ("UniqueID"),
+    ADD FOREIGN KEY ("order_id") REFERENCES ua_4778_manual.orders ("UniqueID"),
+    ADD COLUMN "quantity" INTEGER NOT NULL DEFAULT 1,
+    ADD CONSTRAINT menu_item_order_quantity_positive CHECK ("quantity" > 0);
+
+ALTER TABLE ua_4778_manual.ingredients_suppliers
+    ADD FOREIGN KEY ("ingredient_id") REFERENCES ua_4778_manual.ingredients ("UniqueID"),
+    ADD FOREIGN KEY ("supplier_id") REFERENCES ua_4778_manual.suppliers ("UniqueID");
+
+ALTER TABLE ua_4778_manual.customer_feedback
+    ADD CONSTRAINT customer_feedback_order_id_unique UNIQUE ("order_id"),
+    ADD CONSTRAINT customer_feedback_order_fk FOREIGN KEY ("order_id")
+        REFERENCES ua_4778_manual.orders ("UniqueID");
+
+ALTER TABLE ua_4778_manual.customer_feedback
+    ADD CONSTRAINT customer_feedback_reservation_unique UNIQUE ("reservation_id"),
+    ADD CONSTRAINT customer_feedback_reservation_fk FOREIGN KEY ("reservation_id")
+        REFERENCES ua_4778_manual.reservations ("UniqueID");
